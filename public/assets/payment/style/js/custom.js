cc67f0093b37f811ea7f79d75859ea3470a33b07
RAJANIKANTA BISWAL <rajanikantabiswal15@gmail.com>
2024-12-07 11:53:19 +0530

    public folder deleted

--- public/assets/payment/style/js/custom.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-//Bootstrap Tooltip initialize
-var tooltipTriggerList = [].slice.call(
-  document.querySelectorAll('[data-bs-toggle="tooltip"]')
-);
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl);
-});
